b7378e7ef8e6a0134832884a9582207ff9028eda
mindym1 <76831690+mindy-m@users.noreply.github.com>
2021-09-17 02:27:58 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# Use slices to make the following changes to the cargo_hold list. Be sure to print the list after each step to confirm your work.
+cargo_hold = [1138, 'space suits', 'parrot', 'instruction manual', 'meal packs', 'space tether', '20 meters']
+
+# Insert the string 'keys' at index 3 without replacing any other entries.
+
+# Remove 'instruction manual' from the list. (Hint: The index method is helpful to avoid manually counting an index).
+
+# Replace the elements at indexes 2 - 4 with the items 'cat', 'book', and 'string cheese'.
+
+# Some methods—like append and pop—alter the original list, while others do not. Use the lists supplies_1 and supplies_2 to see if taking a slice or using the ``reverse`` and ``sort`` methods changes the original list.
+supplies_1 = ['duct tape', 'gum', 3.14, False, 6.022e23]
+supplies_2 = ['orange drink', 'nerf toys', 'camera', '42', 'Rutabaga']
+
+# Print a slice of the last 3 items from supplies_1. Does slice alter the original list? Verify this by printing supplies_1 after taking the slice.
+
+
+# reverse the first list, sort the second, and then print both lists. What is the difference between the two methods? Do reverse or sort alter the original lists?
